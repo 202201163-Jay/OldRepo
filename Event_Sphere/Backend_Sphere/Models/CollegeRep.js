b7706bcd8b2c7ec98b205a27cca794120906db36
Smit Godhani <202201162@daiict.ac.in>
2024-09-25 21:33:23 +0530

    Login and logout for both users and colleges (including clubs and committees) have been completed

--- Event_Sphere/Backend_Sphere/Models/CollegeRep.js
+++ Event_Sphere/Backend_Sphere/Models/CollegeRep.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const hashvalue = 10
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
 
 const CollegeRep = new mongoose.Schema({
   repId: {
@@ -17,5 +20,40 @@ const CollegeRep = new mongoose.Schema({
     required: true,
   },
 });
+
+CollegeRep.pre('save', async function (next) {
+  const collegeRep = this;
+  console.log(collegeRep)
+  if (!collegeRep.isModified('password')) return next();
+
+  try {
+      const saltRound = await bcrypt.genSalt(hashvalue);
+      const hashedPassword = await bcrypt.hash(collegeRep.password, saltRound);
+      user.password = hashedPassword;
+      next();
+  } catch (error) {
+      console.error(error);
+      next(error);
+  }
+});
+
+CollegeRep.methods.generateToken = async function () {
+  try {
+      return jwt.sign({
+          name: this.name,
+          userId: this._id.toString(),
+          email: this.email,
+      }, "Smit", {
+          expiresIn: "30d",
+      });
+  } catch (error) { 
+      console.error(error);
+  }
+};
+
+CollegeRep.methods.comparePassword = async function (password) {
+  // console.log(password)
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model("CollegeRep", CollegeRep);
--- Event_Sphere/Frontend_Sphere/src/Pages/Home/Navbar.jsx
+++ Event_Sphere/Frontend_Sphere/src/Pages/Home/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context/AuthProvider";
 
 export const Navbar = () => {
   const [theme, setTheme] = useState(
     localStorage.getItem("theme") ? localStorage.getItem("theme") : "light"
   );
   const element = document.documentElement;
+
+  const {isLoggedIn, username} = useAuth()
+  const {isCollegeRepresentative, setIsCollegeRepresentative} = useAuth()
 
   useEffect(() => {
     if (theme === "dark") {
@@ -40,15 +44,27 @@ export const Navbar = () => {
       <li>
         <Link to="/">Home</Link>
       </li>
-      <li>
-        <Link to="/login">Login</Link>
-      </li>
+      {isLoggedIn ? (
+          <li>
+            <Link to="/logout">Logout</Link>
+          </li>
+      ) : (
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      )}
+      
       <li>
         <Link to="/blogs">Blogs</Link>
       </li>
       <li>
         <a>About</a>
       </li>
+      {isCollegeRepresentative && (
+        <li>
+          <Link to="/listing">List Events</Link>
+        </li>
+      )}
     </>
   );
 
@@ -138,13 +154,27 @@ export const Navbar = () => {
                 <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
               </svg>
             </label>
+            {isLoggedIn? (
+                <div>
+                  <Link
+                    to="/logout"
+                    className="bg-indigo-500 text-white px-3 py-2 rounded-md hover:bg-indigo-700 duration-300"
+                  >
+                    Logout
+                  </Link>
+                </div>
+            ) : (
+              <div>
+                <Link
+                  to="/login"
+                  className="bg-indigo-500 text-white px-3 py-2 rounded-md hover:bg-indigo-700 duration-300"
+                >
+                  Login
+                </Link>
+              </div>
+            )}
             <div>
-              <Link
-                to="/login"
-                className="bg-indigo-500 text-white px-3 py-2 rounded-md hover:bg-indigo-700 duration-300"
-              >
-                Login
-              </Link>
+              {isLoggedIn? (<div>{username}</div>):(<div></div>)}
             </div>
           </div>
         </div>
